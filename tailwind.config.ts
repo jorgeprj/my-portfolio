e0de6034a7123c8ce9fc16e2f463181d8440ad87
jorgeprj <jlmp2002@gmail.com>
2024-02-28 16:33:19 -0300

    init project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: "class",
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#8540c8',
+				sucess: '#41c958',
+				alert: '#d44760',
+
+			},
+			boxShadow: {
+				grid: '0px 4px 10px 0px rgba(0, 0, 0, 0.05);'
+			}
+		}
+	},
+	plugins: [],
 };
 export default config;
